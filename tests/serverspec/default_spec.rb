98cd578627254e82a3051c324497e7974eb88cbf
Tomoyuki Sakurai <y@trombik.org>
2018-11-03 13:38:00 +0900

    feature: initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,49 +1,32 @@
 require "spec_helper"
 require "serverspec"
 
-package = "node_red"
+package = "node-red"
 service = "node_red"
-config  = "/etc/node_red/node_red.conf"
+root_dir = "/opt/node-red"
 user    = "node_red"
 group   = "node_red"
-ports   = [PORTS]
-log_dir = "/var/log/node_red"
-db_dir  = "/var/lib/node_red"
+ports   = [1880]
 
 case os[:family]
 when "freebsd"
-  config = "/usr/local/etc/node_red.conf"
-  db_dir = "/var/db/node_red"
+  root_dir = "/usr/local/node-red"
 end
+config  = "#{root_dir}/settings.js"
 
-describe package(package) do
-  it { should be_installed }
+describe command "npm list -g --depth 0" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/^\+-- node-red@\d+\.\d+\.\d+$/) }
 end
 
 describe file(config) do
+  it { should exist }
   it { should be_file }
-  its(:content) { should match Regexp.escape("node_red") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
+  it { should be_mode 644 }
   it { should be_owned_by user }
   it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/node_red") do
-    it { should be_file }
-  end
+  its(:content) { should match(%r{^// Managed by ansible$}) }
 end
 
 describe service(service) do
